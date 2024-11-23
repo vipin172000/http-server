46f1fa6d80b2d13584ec9568bc461183bc045010
vipin172000 <vipinnkumar143@gmail.com>
2024-11-23 13:25:48 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,8 @@ public class Main {
     
       serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
+            clientSocket.getOutputStream().write(
+          "HTTP/1.1 200 OK\r\n\r\n".getBytes());
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
